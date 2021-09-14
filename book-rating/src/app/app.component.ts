79875f52b4dd9fd34804f08b6b454fb01d686cf7
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-09-14 15:42:42 +0200

    + dashboard

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'br-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
-  title = 'book-rating';
+  title = 'Book Rating';
 }
