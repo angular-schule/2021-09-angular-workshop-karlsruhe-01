d8f01198cc4961f52aed7109fcc49dc8713e6fe0
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-09-21 11:35:31 +0200

    rxjs: async pipe

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,54 +1,15 @@
-import { Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { Subject, ReplaySubject, timer, Subscription } from 'rxjs';
-import { takeWhile, takeUntil, take } from 'rxjs/operators';
+import { takeWhile, takeUntil, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rxw-unsubscribe',
   templateUrl: './unsubscribe.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UnsubscribeComponent implements OnDestroy {
+export class UnsubscribeComponent {
 
-  logStream$ = new ReplaySubject<string | number>();
-  destroyed = false;
-
-  /**
-   * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
-   * Navigiere zurück auf die Startseite und beobachte die Console:
-   * Die Subscription läuft weiter. Wir haben einen klassischen Memory Leak erzeugt...
-   *
-   * Sorge dafür, dass die Subscription in der Methode ngOnDestroy() beendet wird!
-   * Sie wird beim Buttonklick und beim Wegnavigieren ausgelöst.
-   *
-   * Es gibt noch weitere Wege, das Problem zu lösen...
-   */
-  constructor() {
-    const interval$ = timer(0, 1000);
-
-    interval$.pipe(
-
-      /******************************/
-
-      //takeWhile(() => !this.destroyed),
-      take(10)
-      // TODO: takeUntil
-
-      /******************************/
-
-    ).subscribe({
-      next: e => this.log(e),
-      error: err => this.log('❌ ERROR: ' + err),
-      complete: () => this.log('✅ COMPLETE')
-    });
-
-  }
-
-  ngOnDestroy() {
-    this.logStream$.next('DESTROY');
-    this.destroyed = true;
-  }
-
-  log(msg: string | number) {
-    console.log(msg);
-    this.logStream$.next(msg);
-  }
+  interval$ = timer(0, 1000).pipe(
+    tap(console.log)
+  );
 }
