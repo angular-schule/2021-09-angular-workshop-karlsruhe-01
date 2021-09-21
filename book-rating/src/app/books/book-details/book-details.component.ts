b16759624298cbf77ec84a4bb92f6593c1079682
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-09-21 12:18:31 +0200

    rxjs: switchMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { concatMap, map, mergeMap, switchMap } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -12,7 +12,7 @@ export class BookDetailsComponent {
 
   isbn$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    map(isbn => this.bs.getSingleBook(isbn!))
+    switchMap(isbn => this.bs.getSingleBook(isbn!))
   )
 
   constructor(private route: ActivatedRoute,
